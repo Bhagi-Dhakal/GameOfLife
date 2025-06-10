c3f094afa6a6c67d7a4012df3df079261e43d7bd
Bhagi-Dhakal <Bhagirdhakal@gmail.com>
2025-06-10 11:39:05 -0400

    Coments

--- main.cpp
+++ main.cpp
@@ -146,7 +146,7 @@ int main() {
                     }
                 }
             }
-
+            //  asd 
             if (Start.isClicked(event, window)) {
                 game = true;
             }
